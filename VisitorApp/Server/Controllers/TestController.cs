198b9e577f3f2e6f31973ec66e1af1a6673facf8
jhbengtsson <felsdp00@gmail.com>
2021-11-24 21:17:19 +0100

    blazor webassembly kan fetche data fra databasen nu

---
+++ VisitorApp/Server/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VisitorApp.Server.Functions;
+using VisitorApp.Server.Services;
+
+namespace VisitorApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        
+        readonly ITest _ITest;
+
+        public TestController(ITest Itest)
+        {
+            _ITest = Itest;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<List<Test>> TestList()
+        {
+            var test = await _ITest.TestList();
+            return test;
+        }
+    }
+}
---
+++ VisitorApp/Server/Services/TestService.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using VisitorApp.Server.Functions;
+
+namespace VisitorApp.Server.Services
+{
+    public interface ITest
+    {
+        Task<List<Test>> TestList();
+        Task SaveData(Test tests);
+    }
+    public class TestService : ITest
+    {
+        private readonly IConfiguration _configuration;
+
+        public TestService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+        public string GetConnection()
+        {
+            var connection = _configuration.GetSection("ConnectionStrings").GetSection("default").Value;
+            return connection;
+        }
+
+        public async Task<List<Test>> TestList()
+        {
+            var connectionString = this.GetConnection();
+            List<Test> lst = new List<Test>();
+            using (var con = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    await con.OpenAsync();
+                    var com = new MySqlCommand("Select `ID`,`Name` FROM test",
+                        con)
+                    {
+                        CommandType = CommandType.Text
+                    };
+                    var rdr = (MySqlDataReader)await com.ExecuteReaderAsync();
+
+                    while (rdr.Read())
+                    {
+                        lst.Add(new Test
+                        {
+                            ID = (int)rdr["ID"],
+                            Name = rdr["Name"].ToString(),
+                        });
+                    }
+                    return lst.ToList();
+                }
+                finally { con.Close(); }
+
+            }
+        }
+
+        public async Task SaveData(Test students)
+        {
+            var connectionString = this.GetConnection();
+            using (var con = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    await con.OpenAsync();
+                    var com = new MySqlCommand("INSERT INTO tblstudinfo () VALUES (@id,@fname,@mi,@lname,@bdate,@address,@email,@user,@pwd)", con)
+                    {
+                        CommandType = CommandType.Text
+                    };
+                    com.Parameters.AddWithValue("@id", students.ID);
+                    com.Parameters.AddWithValue("@fname", students.Name);
+                   
+                    await com.ExecuteNonQueryAsync();
+                }
+                finally { con.Close(); }
+
+            }
+        }
+
+
+    }
+}
