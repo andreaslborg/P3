0132f7447e38822bc2d5a11c4e501a0808946b57
lasse2507a <44569984+lasse2507a@users.noreply.github.com>
2021-12-06 11:02:10 +0100

    template test working

--- ManagementPages.Tests/CounterCSharpTests.cs
+++ ManagementPages.Tests/CounterCSharpTests.cs
@@ -1,31 +1,35 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using Microsoft.Extensions.DependencyInjection;
+using Bunit;
+using Bunit.TestDoubles;
+using Xunit;
 
-namespace ManagementPages.Tests;
-/// <summary>
-/// These tests are written entirely in C#.
-/// Learn more at https://bunit.dev/docs/getting-started/writing-tests.html#creating-basic-tests-in-cs-files
-/// </summary>
-public class CounterCSharpTests : TestContext
+namespace ManagementPages.Tests
 {
-    [Fact]
-    public void CounterStartsAtZero()
+    public class CounterCSharpTests : TestContext
     {
-        // Arrange
-        var cut = RenderComponent<Counter>();
+        [Fact]
+        public void CounterStartsAtZero()
+        {
+            // Arrange
+            var cut = RenderComponent<Counter>();
 
-        // Assert that content of the paragraph shows counter at zero
-        cut.Find("p").MarkupMatches("<p>Current count: 0</p>");
-    }
+            // Assert that content of the paragraph shows counter at zero
+            cut.Find("p").MarkupMatches("<p>Current count: 0</p>");
+        }
 
-    [Fact]
-    public void ClickingButtonIncrementsCounter()
-    {
-        // Arrange
-        var cut = RenderComponent<Counter>();
+        [Fact]
+        public void ClickingButtonIncrementsCounter()
+        {
+            // Arrange
+            var cut = RenderComponent<Counter>();
 
-        // Act - click button to increment counter
-        cut.Find("button").Click();
+            // Act - click button to increment counter
+            cut.Find("button").Click();
 
-        // Assert that the counter was incremented
-        cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+            // Assert that the counter was incremented
+            cut.Find("p").MarkupMatches("<p>Current count: 1</p>");
+        }
     }
 }
