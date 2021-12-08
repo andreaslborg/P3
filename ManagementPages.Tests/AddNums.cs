cfb69c36f8201d46d13fc774d630bd39433723ad
Mikkel <mdolme20@student.aau.dk>
2021-12-08 10:46:37 +0100

    simple test

---
+++ ManagementPages.Tests/AddNums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ManagementPages.Tests
+{
+    public class AddNums
+    {
+        public AddNums()
+        {
+        }
+
+        public int AddNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ ManagementPages.Tests/TestMustHave2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+using Microsoft.Extensions.DependencyInjection;
+using Bunit;
+using Bunit.TestDoubles;
+using Xunit;
+using ManagementPages.UI.Components;
+using ManagementPages.Model;
+
+namespace ManagementPages.Tests
+{
+    public class TestMustHave2 : TestContext
+    {
+        [Fact]
+        public void DoesNumbersAddUp()
+        {
+            // Arrange
+            AddNums addNums = new AddNums();
+            int input1 = 10;
+            int input2 = 30;
+            int expected = 40;
+
+            int result = addNums.AddNumbers(input1, input2);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
