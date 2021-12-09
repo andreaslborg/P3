89cd87356aedcb8e8c2737c628ee652554db9225
mariusgardshodn <72624369+mariusgardshodn@users.noreply.github.com>
2021-12-09 15:12:24 +0100

    yes

---
+++ VisitorApplication.Tests/CategoryOrderTest.cs
@@ -0,0 +1,23 @@
+﻿using Bunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisitorApplication.Client.UI.Pages;
+using VisitorApplication.Shared;
+using VisitorApplication.Client;
+using VisitorApplication.Server;
+
+namespace VisitorApplication.Tests
+{
+    class CategoryOrderTest : TestContext
+    {
+        List<int> CategoryOrder { get; set; } = new();
+
+        Informationboard SelectedInformationboard { get; set; } = new();
+
+        
+        
+    }
+}
