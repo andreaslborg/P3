e9aaecb732ae97e6c1ee8f50f0492a7602af9c1e
mariusgardshodn <72624369+mariusgardshodn@users.noreply.github.com>
2021-12-09 15:56:53 +0100

    CategoryOrderTest Finished

--- VisitorApplication.Tests/CategoryOrderTest.cs
+++ VisitorApplication.Tests/CategoryOrderTest.cs
@@ -6,18 +6,58 @@ using System.Text;
 using System.Threading.Tasks;
 using VisitorApplication.Client.UI.Pages;
 using VisitorApplication.Shared;
-using VisitorApplication.Client;
-using VisitorApplication.Server;
+using Xunit;
 
 namespace VisitorApplication.Tests
 {
-    class CategoryOrderTest : TestContext
+    public class CategoryOrderTest : TestContext
     {
-        List<int> CategoryOrder { get; set; } = new();
+        [Fact]
+        public void TestCategoryOrder()
+        {
+            List<int> CategoryOrder;
 
-        Informationboard SelectedInformationboard { get; set; } = new();
+            Informationboard SelectedInformationboard = new Informationboard();
 
-        
-        
+            SelectedInformationboard.CategoryOrder = "223, 144, 342, 412, 667, 554";
+
+            CategoryOrder = SplitCategoriesToList(SelectedInformationboard);
+
+            List<int> expectedCategoryOrder = new List<int> { 223, 144, 342, 412, 667, 554 };
+
+            Assert.Equal(expectedCategoryOrder, CategoryOrder);
+        }
+
+        public List<int> SplitCategoriesToList(Informationboard informationboard)
+        {
+            List<string> stringIDList = informationboard.CategoryOrder.Split(",").ToList();
+            List<int> intIDList = new();
+
+            foreach (string item in stringIDList)
+            {
+
+                try
+                {
+                    intIDList.Add(Convert.ToInt32(item));
+                }
+                catch (FormatException)
+                {
+                }
+            }
+
+            return intIDList;
+        }
+
     }
 }
+
+
+/* 
+ public int InformationboardID { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string QRCode { get; set; }
+        public bool IsPublished { get; set; }
+        public int LicenseID { get; set; }
+        public string CategoryOrder { get; set; }
+ */
