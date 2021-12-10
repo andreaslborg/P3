6d8186f4021518502b78abeb1f35f373126bc998
mariusgardshodn <72624369+mariusgardshodn@users.noreply.github.com>
2021-12-10 10:10:01 +0100

    ConvertToCommaSeperatedString Test Finished

--- ManagementPages.Tests/CheckCategoryOrderTest.cs
+++ ManagementPages.Tests/CheckCategoryOrderTest.cs
@@ -5,15 +5,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using ManagementPages.Model;
 
 namespace ManagementPages.Tests
 {
-    public class CheckCategoryOrderTest : TestContext
+    public class CheckCategoryOrderTest
     {
+        Dictionary<int, ICategoryModel> Categories = new();
+        List<int> CategoryOrder = new List<int> { 223, 144, 342, 412, 667, 554 };
+
+
         [Fact]
-        public void TestCheckCategoryOrder()
+        public void TestCategoryOrder()
         {
+            InformationBoardDataModel informationBoardDataModel = new InformationBoardDataModel();
+            
+
 
         }
-    }
+
+
+        public void CheckCategoryOrder()
+        {
+            // check if all categories are in the CategoryOrder (meaning that they will be displayed), and
+            // add them to the end, if they are missing
+            List<int> keysToAdd = new();
+            List<int> keysToRemove = new();
+
+            foreach (var category in Categories)
+                if (!CategoryOrder.Contains(category.Key))
+                    keysToAdd.Add(category.Key);
+
+            // check if there are any invalid ids in the CategoryOrder, and if so - delete them
+            foreach (var key in CategoryOrder)
+                if (!Categories.ContainsKey(key))
+                    keysToRemove.Add(key);
+
+            // keys cannot be deleted/added inside foreach loop as it messes up the order
+            foreach (var key in keysToAdd) CategoryOrder.Add(key);
+
+            foreach (var key in keysToRemove) CategoryOrder.Remove(key);
+        }
+
+
+    }    
 }
+
+        
--- ManagementPages.Tests/ConvertCommaToSeperatedStringTest.cs
+++ ManagementPages.Tests/ConvertCommaToSeperatedStringTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using ManagementPages.Model;
 
 namespace ManagementPages.Tests
 {
@@ -12,7 +13,25 @@ namespace ManagementPages.Tests
         [Fact]
         public void TestConvertCommaToSeperatedString()
         {
+            InformationBoardDataModel informationBoardDataModel = new InformationBoardDataModel();
+            List<int> CategoryOrder = new List<int> { 223, 144, 342, 412, 667, 554 };
 
+            informationBoardDataModel.CategoryOrder = ConvertToCommaSeparatedString(CategoryOrder);
+
+            var expectedCategoryOrderString = "223,144,342,412,667,554,";
+
+            Assert.Equal(expectedCategoryOrderString, informationBoardDataModel.CategoryOrder);
+        }
+
+        string ConvertToCommaSeparatedString(List<int> list)
+        {
+            var result = string.Empty;
+
+            foreach (var number in list) result += $"{number},";
+
+            return result;
         }
     }
 }
+
+
