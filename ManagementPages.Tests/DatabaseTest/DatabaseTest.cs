aada6060e909459a4012ebae5ab493042b5a93a2
Mikkel <mdolme20@student.aau.dk>
2021-12-15 10:05:48 +0100

    db tests

--- ManagementPages.Tests/DatabaseTest/DatabaseTest.cs
+++ ManagementPages.Tests/DatabaseTest/DatabaseTest.cs
@@ -40,5 +40,48 @@ namespace ManagementPages.Tests
             objectFound = testList.Any(item => item.TestInt == dbObject.TestInt && item.TestString == dbObject.TestString && item.TestBool == dbObject.TestBool);
             Assert.False(objectFound);
         }
+
+        [Fact]
+        public async Task AddDbTest()
+        {
+            DbService dbService = new DbService(configuration);
+
+            DbTestClass dbObject = new DbTestClass
+            {
+                TestInt = 1,
+                TestString = "This is a test string",
+                TestBool = true
+            };
+
+            var sqlInsert = "Insert into Test (TestInt, TestString, TestBool) values (1, \"This is a test string\", true);";
+            var sqlSelect = "Select * from Test;";
+
+            await dbService.SaveData(sqlInsert, dbObject);
+            var testList = await dbService.LoadData<DbTestClass, dynamic>(sqlSelect, new { });
+            bool objectFound = testList.Any(item => item.TestInt == dbObject.TestInt && item.TestString == dbObject.TestString && item.TestBool == dbObject.TestBool);
+            Assert.True(objectFound);
+        }
+
+        // Test only works if database has exactly one row where TestInt = 1, TestString = "This is a test string", TestBool = true.
+        [Fact]
+        public async Task DeleteDbTest()
+        {
+            DbService dbService = new DbService(configuration);
+
+            DbTestClass dbObject = new DbTestClass
+            {
+                TestInt = 1,
+                TestString = "This is a test string",
+                TestBool = true
+            };
+
+            var sqlDelete = "Delete from Test Where TestInt = 1;";
+            var sqlSelect = "Select * from Test;";
+
+            await dbService.SaveData(sqlDelete, dbObject);
+            var testList = await dbService.LoadData<DbTestClass, dynamic>(sqlSelect, new { });
+            bool objectFound = testList.Any(item => item.TestInt == dbObject.TestInt && item.TestString == dbObject.TestString && item.TestBool == dbObject.TestBool);
+            Assert.False(objectFound);
+        }
     }
 }
