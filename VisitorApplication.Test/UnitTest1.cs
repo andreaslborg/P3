f967e2116a3fc6607eae838b93e181c6824bc0a3
mariusgardshodn <72624369+mariusgardshodn@users.noreply.github.com>
2021-12-15 10:05:04 +0100

    xUnit test projects created

---
+++ VisitorApplication.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace VisitorApplication.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
