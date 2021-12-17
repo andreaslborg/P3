94970da19c6d6e8c197ea88ecf03e6f26936c5ce
mariusgardshodn <72624369+mariusgardshodn@users.noreply.github.com>
2021-12-17 09:44:55 +0100

    DeletePostFromDatabaseTest finished

--- ManagementPages.Test/UnitTests/DeletePostFromDatabaseTest.cs
+++ ManagementPages.Test/UnitTests/DeletePostFromDatabaseTest.cs
@@ -13,7 +13,7 @@ namespace ManagementPages.Test.UnitTests
     public class DeletePostFromDatabaseTest
     {
         [Fact]
-        public void TestDeletePostFromDatabase()
+        public async void TestDeletePostFromDatabase()
         {
             // Arrange
             // Insert from management
@@ -23,21 +23,37 @@ namespace ManagementPages.Test.UnitTests
             .Build();
 
             IDbService dbService = new DbService(configuration);
-            IPostModel SelectedPost = new PostModel();
+            IPostModel selectedPost = new PostModel();
 
-            var categoryInitial = new PostDataModel
-            {
-                PostId = 200, CategoryId = 1, Title = "Test title",
+            var postInitial = new PostDataModel { CategoryId = 1, Title = "Test title",
                 Text = "Test text", Author = "Tester", IsPublished = true, Link = "Test Link" };
 
-            var sqlInsert2 = $"insert into Post (Title, CategoryId, Text, Author, IsPublished, Link) values (\"{categoryInitial.Title}\", {categoryInitial.CategoryId}, \"{categoryInitial.Text}\", \"{categoryInitial.Author}\", {categoryInitial.IsPublished}, \"{categoryInitial.Link}\" );";
-            dbService.SaveData(sqlInsert2, categoryInitial);
+            selectedPost.PostDataModel = postInitial;
+
+            var sqlInsert2 = $"insert into Post (Title, CategoryId, Text, Author, IsPublished, Link) values (\"{postInitial.Title}\", {postInitial.CategoryId}, \"{postInitial.Text}\", \"{postInitial.Author}\", {postInitial.IsPublished}, \"{postInitial.Link}\" );";
+            var sqlSelect = "Select * from Post;";
+
+            await dbService.SaveData(sqlInsert2, postInitial);
+
+            var AllPostsList = await dbService.LoadData<PostDataModel, dynamic>(sqlSelect, new { });
+
+            // This boolean shows that the post has been saved in the database
+            bool objectFoundAfterAddingPost = AllPostsList.Any(item => item.Title == postInitial.Title);
+
+            var testPost = AllPostsList.Where(item => item.Title == "Test title").First();
+
+            selectedPost.PostDataModel.PostId = testPost.PostId;
 
             // Act
+            await selectedPost.DeletePostFromDatabase(dbService);
 
+            AllPostsList = await dbService.LoadData<PostDataModel, dynamic>(sqlSelect, new { });
+
+            // This boolean show that the post has been deleted from the database
+            bool objectFoundAfterDeletingPost = AllPostsList.Any(item => item.Title == postInitial.Title);
 
             // Assert
+            Assert.NotEqual(objectFoundAfterAddingPost, objectFoundAfterDeletingPost);
         }
-
     }
 }
